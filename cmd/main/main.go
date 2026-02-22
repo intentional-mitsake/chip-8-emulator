6b04dedb138166de631b5ee9b7624bc8ff658d99
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-02-22 12:25:29 +0545

    mem map commnt

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,8 +5,16 @@ import (
 	"fmt"
 )
 
+//MEMORY MAP for the CHIP-8 system:
+//0-79 --> fontset(0-F chars)--> load after initializing the Chip8 struct
+//80-511 --> Interpreter/Resrved
+//512-4095 --> Program/ROM/Stack/General purpose registers/Index register/Timers/Keypad/Display
+
 func main() {
 	chip8 := def.NewChip8()
+	chip8.LoadFontset() //load the fontset first before loading the ROM
+	//%v prints value in a default format
+	//%+v prints struct with field names
 	//fmt.Printf("Initialized Chip8: %+v\n", chip8)
 	romPath := "./pkg/assets/PONG"
 	err := chip8.LoadROM(romPath)
