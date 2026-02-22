38346f52a358ffa3e1ecf69aa8779dcc1e7c0827
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-02-22 15:03:31 +0545

    modificaitoins

--- pkg/c8-def/instructions.go
+++ pkg/c8-def/instructions.go
@@ -1,47 +1,47 @@
 package c8def
 
 type InstructionSet struct {
-	//map for key->vlaue pairs of instruction name
-	Set map[string]string //All 34 instructions in the CHIP-8 instruction set
+	// Map instruction name -> base opcode
+	Set map[string]uint16
 }
 
 func NewInstructionSet() *InstructionSet {
 	return &InstructionSet{
-		Set: map[string]string{
-			"Clear":     "00E0", // Clear the display
-			"Return":    "00EE", // Return from a subroutine
-			"Jump":      "1NNN", // Jump to address NNN
-			"Call":      "2NNN", // Call subroutine at NNN
-			"SkipEq":    "3XNN", // Skip next instruction if VX == NN
-			"SkipNe":    "4XNN", // Skip next instruction if VX != NN
-			"SkipEqV":   "5XY0", // Skip next instruction if VX == VY
-			"Set":       "6XNN", // Set VX = NN
-			"Add":       "7XNN", // Add NN to VX
-			"Mov":       "8XY0", // Set VX = VY
-			"Or":        "8XY1", // VX |= VY
-			"And":       "8XY2", // VX &= VY
-			"Xor":       "8XY3", // VX ^= VY
-			"AddV":      "8XY4", // VX += VY, VF = carry
-			"Sub":       "8XY5", // VX -= VY, VF = NOT borrow
-			"Shr":       "8XY6", // VX >>= 1, VF = least significant bit
-			"SubN":      "8XY7", // VX = VY - VX, VF = NOT borrow
-			"Shl":       "8XYE", // VX <<= 1, VF = most significant bit
-			"SkipNeV":   "9XY0", // Skip next instruction if VX != VY
-			"SetI":      "ANNN", // Set I = NNN
-			"Jump0":     "BNNN", // Jump to NNN + V0
-			"Rand":      "CXNN", // VX = random byte AND NN
-			"Sprite":    "DXYN", // Draw sprite at (VX,VY), VF = collision
-			"KeyEq":     "EX9E", // Skip next instruction if key VX pressed
-			"KeyNe":     "EXA1", // Skip next instruction if key VX not pressed
-			"GetDelay":  "FX07", // Set VX = delay timer value
-			"WaitKey":   "FX0A", // Wait for keypress, store in VX
-			"SetDelay":  "FX15", // Set delay timer = VX
-			"SetBuzzer": "FX18", // Set sound timer = VX
-			"AddI":      "FX1E", // I += VX
-			"Hex":       "FX29", // Set I = location of sprite for digit VX
-			"Bcd":       "FX33", // Store BCD of VX in memory[I..I+2]
-			"Save":      "FX55", // Store V0..VX in memory starting at I
-			"Load":      "FX65", // Read V0..VX from memory starting at I
+		Set: map[string]uint16{
+			"Clear":     0x00E0, // Clear the display
+			"Return":    0x00EE, // Return from a subroutine
+			"Jump":      0x1000, // Jump to address NNN
+			"Call":      0x2000, // Call subroutine at NNN
+			"SkipEq":    0x3000, // Skip next instruction if VX == NN
+			"SkipNe":    0x4000, // Skip next instruction if VX != NN
+			"SkipEqV":   0x5000, // Skip next instruction if VX == VY
+			"Set":       0x6000, // Set VX = NN
+			"Add":       0x7000, // Add NN to VX
+			"Mov":       0x8000, // Set VX = VY
+			"Or":        0x8001, // VX |= VY
+			"And":       0x8002, // VX &= VY
+			"Xor":       0x8003, // VX ^= VY
+			"AddV":      0x8004, // VX += VY, VF = carry
+			"Sub":       0x8005, // VX -= VY, VF = NOT borrow
+			"Shr":       0x8006, // VX >>= 1, VF = least significant bit
+			"SubN":      0x8007, // VX = VY - VX, VF = NOT borrow
+			"Shl":       0x800E, // VX <<= 1, VF = most significant bit
+			"SkipNeV":   0x9000, // Skip next instruction if VX != VY
+			"SetI":      0xA000, // Set I = NNN
+			"Jump0":     0xB000, // Jump to NNN + V0
+			"Rand":      0xC000, // VX = random byte AND NN
+			"Sprite":    0xD000, // Draw sprite at (VX,VY), VF = collision
+			"KeyEq":     0xE09E, // Skip next instruction if key VX pressed
+			"KeyNe":     0xE0A1, // Skip next instruction if key VX not pressed
+			"GetDelay":  0xF007, // Set VX = delay timer value
+			"WaitKey":   0xF00A, // Wait for keypress, store in VX
+			"SetDelay":  0xF015, // Set delay timer = VX
+			"SetBuzzer": 0xF018, // Set sound timer = VX
+			"AddI":      0xF01E, // I += VX
+			"Hex":       0xF029, // Set I = location of sprite for digit VX
+			"Bcd":       0xF033, // Store BCD of VX in memory[I..I+2]
+			"Save":      0xF055, // Store V0..VX in memory starting at I
+			"Load":      0xF065, // Read V0..VX from memory starting at I
 		},
 	}
 }
